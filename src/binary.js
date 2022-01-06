99acc17b06d54392eb7f4f2458646f782821b795
Bulat Masalimov <bmasalimov5@gmail.com>
2022-01-06 18:05:36 +0200

    Revert "Revert "binary-search is done""
    This reverts commit 881797d71ccbdf7c07a56bb7073557fc43753c50.

--- src/binary.js
+++ src/binary.js
@@ -1,16 +1,63 @@
-// import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync';
 
-export default function binarySearch(mas, index) {
-  let high = mas.length - 1;
-  let low = 0;
-  while (low <= high) {
-    const mid = (low + high) / 2;
+function getIndex() {
+  const index = readlineSync.question('Print Index: ');
+  // console.log(index);
+  return index;
+}
+
+function getMas(index) {
+  const mas = [];
+  const lengthOfmas = readlineSync.question('Print length of massive: ');
+  if ((lengthOfmas - 1) < index) return null;
+  for (let i = 0; i < lengthOfmas; i += 1) {
+    const getElement = `Print ${i} element of massive: `;
+    const element = +readlineSync.question(getElement);
+    mas.push(element);
+  }
+  return mas;
+}
+
+function sortMas(mas) {
+  if (mas === null) return null;
+  const masSorted = [];
+  /* eslint-disable-next-line */
+  for (const key of mas) {
+    masSorted.push(key);
+  }
+  console.log(`Unsorted massive: [${masSorted}]`);
+  for (let j = masSorted.length - 1; j > 0; j -= 1) {
+    for (let i = 0; i < j; i += 1) {
+      if (masSorted[i] > masSorted[i + 1]) {
+        const cyrcle = masSorted[i];
+        masSorted[i] = masSorted[i + 1];
+        masSorted[i + 1] = cyrcle;
+      }
+    }
+  }
+  console.log(`Sorted massive: [${masSorted}]`);
+  return masSorted;
+}
+
+export default function binarySearch() {
+  const index = getIndex();
+  // console.log(index);
+  const mas = sortMas(getMas(index));
+  // console.log(mas);
+  if (mas === null) return 'index over length of massive, try again';
+  let highIndex = mas.length - 1;
+  let lowIndex = 0;
+  while (lowIndex <= highIndex) {
+    const mid = Math.floor((lowIndex + highIndex) / 2);
+    // console.log(mid);
     const guess = mas[mid];
-    if (guess === mas[index]) return mid;
-    if (guess > mas[index]) high = mid;
-    if (guess < mas[index]) low = mid;
+    // console.log(guess);
+    if (guess === mas[index]) return guess;
+    if (guess > mas[index]) {
+      highIndex = mid;
+    } else {
+      lowIndex = mid;
+    }
   }
   return null;
 }
-// cоздать функцию по созданию массива
-// создать функцию по запросу индекса

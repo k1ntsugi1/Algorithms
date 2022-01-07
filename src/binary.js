dc8e2fcd9fdd68aa2df122a52fba5fe256db37f2
Bulat Masalimov <bmasalimov5@gmail.com>
2022-01-07 15:22:47 +0200

    have done easier finding highIndex and lowIndex

--- src/binary.js
+++ src/binary.js
@@ -18,16 +18,11 @@ export default function binarySearch() {
   while (lowIndex <= highIndex) {
     const midIndex = Math.floor((lowIndex + highIndex) / 2);
     const guess = mas[midIndex];
-    console.log(guess);
     if (guess === correctItem) return (`index of item: ${midIndex}`);
     if (guess > correctItem) {
-      /* eslint-disable-next-line */
-      midIndex % 2 === 0 ? highIndex = midIndex : highIndex = midIndex - 1; // if index === 1 I have to decrease index on 1 point else I can't reach first index;
-      console.log(`${highIndex} - highIndex`);
+      highIndex = midIndex - 1; // else I can't reach first index;
     } else {
-      /* eslint-disable-next-line */
-      midIndex % 2 === 0 ? lowIndex = midIndex : lowIndex = midIndex + 1; // else I can't reach last index because of Math.floor;
-      console.log(`${lowIndex} - lowIndex`);
+      lowIndex = midIndex + 1; // else I can't reach last index because of Math.floor;
     }
     if (Math.log2(mas.length) < counterCircle) break;
     counterCircle += 1;
